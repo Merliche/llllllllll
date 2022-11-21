ba24eaeac16b4655d2997102b49b68b794bd33bc
Merliche <113665026+Merliche@users.noreply.github.com>
2022-11-21 21:59:08 +0100

    Initial commit

---
+++ identification.py
@@ -0,0 +1,80 @@
+from tkinter import *
+from tkinter import messagebox
+import sqlite3
+from functools import partial
+import interfacegraphique
+import sys
+
+
+conn = sqlite3.connect('my_DB.db')
+cur = conn.cursor()
+cur.execute("""SELECT solde FROM utilisateurs """)
+user_solde = cur.fetchall()
+cur.execute("""SELECT last_retrait FROM utilisateurs""")
+user_historique = cur.fetchall()
+
+def check_if_exist(identifiants):
+    conn = sqlite3.connect('my_DB.db')
+    cur = conn.cursor()
+    cur.execute("""SELECT id FROM utilisateurs WHERE CB=?""",(identifiants[0],))
+    id = cur.fetchone()
+    cur.execute("""SELECT CB,code_pin FROM utilisateurs WHERE id=?""", id,)
+    response = cur.fetchone()
+    user_identifiants = list(response)
+    cur.execute("""SELECT Name FROM utilisateurs WHERE id=?""", id,)
+    NomPrenom = cur.fetchone()
+    texte = "Bienvenue"+ str(NomPrenom)+ "!"
+    if identifiants[1] == user_identifiants[1]:
+        messagebox.showinfo(title=None, message= texte)
+        return "toto"
+    else:
+        sys.exit()
+
+
+
+
+
+OptionList = [
+"5136405006383498",
+"3285548746932323",
+"5764831715489314",
+"5434 8952 2314 7594",
+"5129 0309 5703 7668",
+"5395 3319 9090 6720"
+] 
+
+
+def validateLogin(CB, password):
+    CB_values = CB.get()
+    password_values = password.get()
+    print("username entered :", CB.get())
+    print("password entered :", password.get())
+    identifiants = [CB_values,password_values]
+    print(identifiants)
+    if check_if_exist(identifiants) == "toto":
+        Window.destroy()
+    else:
+        messagebox.showerror("error", "Wrong Code")
+    return identifiants
+
+#window
+Window = Tk()  
+Window.geometry('400x150')  
+Window.title('Tkinter Login Form - pythonexamples.org')
+
+#username label and text entry box
+CBLabel = Label(Window, text="CB").grid(row=0, column=0)
+CB = StringVar()
+CBEntry = OptionMenu(Window,CB,*OptionList).grid(row=0, column=1)  
+
+#password label and password entry box
+passwordLabel = Label(Window,text="Password").grid(row=1, column=0)  
+password = StringVar()
+passwordEntry = Entry(Window, textvariable=password, show='*').grid(row=1, column=1)  
+
+validateLogin = partial(validateLogin, CB, password)
+
+#login button
+loginButton = Button(Window, text="Login", command=validateLogin).grid(row=4, column=0)  
+
+Window.mainloop()
